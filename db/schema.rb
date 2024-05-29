310ce634253c221e70b2a86e8dcb0e2801716868
nswdry <marin.taniguchi25@gmail.com>
2024-05-29 22:16:24 +0900

    add:TaskテーブルのnameカラムにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_25_074742) do
+ActiveRecord::Schema.define(version: 2024_05_28_135952) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
