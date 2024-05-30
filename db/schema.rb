d3c2798b4c1ef38c2b02c3ae35b9b581486d3b4b
nswdry <marin.taniguchi25@gmail.com>
2024-05-30 22:15:27 +0900

    add:管理者機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_30_123852) do
+ActiveRecord::Schema.define(version: 2024_05_30_130328) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2024_05_30_123852) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
